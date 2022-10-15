d32d4f92321fc99b175bea71691f200152e834dc
Alexander Upton <45669411+alexanderupton@users.noreply.github.com>
2022-10-15 17:45:13 -0400

    Add files via upload
    replacing setup with install for user clarity

---
+++ vert_arcade_only_install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# vert_arcade_only_setup.sh : v0.01 : Alexander Upton : 10/08/2022
+
+# Copyright (c) 2022 Alexander Upton <alex.upton@gmail.com>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# You can download the latest version of this script from:
+# https://github.com/alexanderupton/vert_arcade_only
+
+# ========= CODE STARTS HERE =========
+if [ -f /media/fat/Scripts/vert_arcade_only.sh ]; then
+ /media/fat/Scripts/vert_arcade_only.sh -i
+else
+ wget --no-check-certificate -O /media/fat/Scripts/vert_arcade_only.sh https://raw.githubusercontent.com/alexanderupton/vert_arcade_only/main/vert_arcade_only.sh
+ if [ -f /media/fat/Scripts/vert_arcade_only.sh ]; then
+  /media/fat/Scripts/vert_arcade_only.sh -i
+ else
+  echo "vert_arcade_only.sh is not installed in /media/fat/Scripts"
+  echo "Download: https://github.com/alexanderupton/vert_arcade_only"
+ fi
+fi
+
+exit 0
